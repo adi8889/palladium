a702ea6419b6121af4fe7a0abe18d4a7391e1ff1
ADITYA <adityah.1si21cs002@gmail.com>
2025-05-13 12:14:50 +0530

    Add student profiles with detailed bios and timestamps

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { z } from "zod";
+import { periodicTable } from "./periodic-table.js";
+import { students } from "./student.js";
+
+
 const mcpServer = new McpServer({
   name: "Palladium",
   version: "1.0.0",
 });
-//1. Resources | get -> files
-mcpServer.resource("Palladium", "palladium://details", async (uri, extras) => {
+
+// 1. Resources | GET -> Files
+
+mcpServer.resource("Palladium Metal", "palladium://details", async (uri, extras) => {
   return {
     contents: [
       {
@@ -22,18 +29,83 @@ mcpServer.resource("Palladium", "palladium://details", async (uri, extras) => {
           boilingPointK: 3236,
           density: 12.02,
           description:
-            `Palladium is a rare and lustrous silvery-white metal discovered in 1803 by
-William Hyde Wollaston. It belongs to the platinum group metals (PGMs)
-and is highly valued for its excellent catalytic properties. Palladium
-is primarily used in catalytic converters for automobiles, electronics,
-dentistry, and hydrogen purification. It is chemically stable, resistant
-to corrosion, and plays a vital role in clean energy technologies due
-to its ability to absorb hydrogen.`,
+            "Palladium is a rare and lustrous silvery-white metal discovered in 1803 by William Hyde Wollaston. It belongs to the platinum group metals (PGMs) and is highly valued for its excellent catalytic properties. Palladium is primarily used in catalytic converters for automobiles, electronics, dentistry, and hydrogen purification. It is chemically stable, resistant to corrosion, and plays a vital role in clean energy technologies due to its ability to absorb hydrogen.",
         }),
-        mimeType: "application.json",
+        mimeType: "application/json",
       },
     ],
   };
 });
+
+// 2. Tools | POST -> Files | Side-Effects
+mcpServer.tool(
+  "Elements",
+  "Details about all the elements in the world",
+  {
+    name: z.string().min(1).describe("Name of the metal that you want the data for"),
+  },
+  ({ name }) => {
+    const element = periodicTable.find((element) => element.name.toLowerCase() === name.toLocaleLowerCase());
+
+    if (!element) {
+      return {
+        content: [
+          {
+            type: "text",
+            text: JSON.stringify({
+              error: `Element with name "${name}" not found!`,
+            }),
+          },
+        ],
+      };
+    }
+
+    return {
+      content: [
+        {
+          type: "text",
+          text: JSON.stringify(element),
+        },
+      ],
+    };
+  },
+);
+
+mcpServer.tool(
+  "People",
+  "Quick bio of people",
+  {
+    name: z.string().min(1).describe("Name of the person that you want the bio for"),
+  },
+  ({ name }) => {
+    const student = students.find((student: { Name: string | string[]; }) => {
+      return student.Name.includes(name.toLowerCase());
+    });
+
+    if (!student) {
+      return {
+        content: [
+          {
+            type: "text",
+            text: JSON.stringify({
+              error: `Student with name "${name}" not found!`,
+            }),
+          },
+        ],
+      };
+    }
+
+    return {
+      content: [
+        {
+          type: "text",
+          text: JSON.stringify(student),
+        },
+      ],
+    };
+  },
+);
+
 const transport = new StdioServerTransport();
+
 await mcpServer.connect(transport);
---
+++ src/student.ts
@@ -0,0 +1,114 @@
+[
+  {
+    Timestamp: "2025/05/13 11:13:57 AM GMT+5:30",
+    Name: "Jagadish C K",
+    Bio: "I'm a final year student pursuing Information Science and Engineering at Siddaganga Institute of Technology, with a deep passion for web development and building impactful digital solutions.I’ve worked on full-stack projects using React, Tailwind CSS, Node.js, Hono, Prisma, and PostgreSQL, and also have solid programming experience in C++, Python, and TypeScript. Some of my key projects include a weather application, a food ordering system, and a CPU scheduling visualizer—each designed with an emphasis on performance and user experience. I'm currently looking for opportunities in web development or software engineering where I can apply my skills, contribute to real-world projects, and continue to grow as a developer.",
+  },
+  {
+    Timestamp: "2025/05/13 11:14:11 AM GMT+5:30",
+    Name: "Kabir Asani",
+    Bio: "A full-stack mobile developer by experience who believes in the wholesome integration of technology with liberal arts.  I develop mobile applications in Flutter, React Native, SwiftUI and Compose. I develop web applications in Next.js and Svelte. I develop backend applications (REST APIs) in NodeJS/BunJS and PostgreSQL/MongoDB.  I've worked primarily in high-growth product startups aimed at creating a lasting difference. Insatiably curious as I am, I've devoured as much practical knowledge as possible in all the workplaces I've been part of.   Recently ventured into entrepreneurship with AI Shorts - https://aishorts.club. Personal Website - https://kabirasani.com",
+  },
+  {
+    Timestamp: "2025/05/13 11:14:22 AM GMT+5:30",
+
+    Name: "Suhas Y Sunkad",
+    Bio: "I am training in slack bootcamp of purplehshorts as a software developer",
+  },
+  {
+    Timestamp: "2025/05/13 11:15:03 AM GMT+5:30",
+
+    Name: "Shardamani K N",
+    Bio: "I am a final year engineering student studying Information Science. I find technology to be the most exiting field to learn and explore new innovations that has been happening around us. I aspire to pursue M. Tech in this field of Computer Science to deepen my knowledge and continue to do so.",
+  },
+  {
+    Timestamp: "2025/05/13 11:15:04 AM GMT+5:30",
+
+    Name: "Jyothi Swaroop N",
+    Bio: "I'm an engineering student in my 8th semester with a strong focus on fullstack web development and backend engineering. I work primarily with TypeScript, using frameworks like Node.js (Hono and Express) on the backend and React on the frontend. My recent projects include a College Database Management System and a UPI fraud detection system, both involving robust database design using Prisma and Neon.tech.  I'm also interested in creating content anonymously. One of my side ventures includes producing animated YouTube videos featuring classic rhymes, aiming to build a monetizable channel without revealing my identity. I enjoy building systems end-to-end and constantly seek ways to turn my technical skills into real-world solutions.",
+  },
+  {
+    Timestamp: "2025/05/13 11:15:26 AM GMT+5:30",
+
+    Name: "Thanmayi J R",
+    Bio: "I'm currently pursuing a degree in Bachelor of Engineering in Information Science from Siddaganga Institute Of Technology. I'm a curious and keen learner, always eager to explore new technologies and concepts.  Right now, I'm gaining hands-on experience through an internship at Stack Lane by Purple Shorts, where I'm learning how real-world software development works and sharpening my technical skills.  My future goal is to become a skilled Software Engineer, building solutions that make a real impact. This field continuously inspires me to dive deeper, grow my knowledge, and work towards a successful and fulfilling career.",
+  },
+  {
+    Timestamp: "2025/05/13 11:15:55 AM GMT+5:30",
+
+    Name: "Kethan gowda N",
+    Bio: "I’m Kethan Gowda, a Computer Science and Engineering student at Siddaganga Institute of Technology (SIT), Tumkur. I’m passionate about coding, building impactful software, and continuously learning new technologies. I enjoy working across the full stack—developing both backend systems and frontend interfaces—and have a growing interest in AI and DevOps. I'm currently honing my skills as a Fullstack AI Developer Intern at StackLane, where I’ve been working on real-world projects involving web development, authentication systems, containerization, and AI-powered applications.",
+  },
+  {
+    Timestamp: "2025/05/13 11:15:58 AM GMT+5:30",
+
+    Name: "Mahesh R",
+    Bio: "I'm Mahesh, an Information Science and Engineering student with a growing passion for full-stack development and applied AI. I work with a modern tech stack—Node.js, Hono.js, Prisma, Next.js, React.js, Tailwind CSS, and Radix UI—and have practical experience with databases like NeonDB and Supabase. I'm focused on building scalable, user-first applications while diving deeper into DevOps workflows using Docker, GitHub Actions, and cloud platforms. Outside of tech, I enjoy reading Nicholas Sparks novels, watching test cricket, going on short runs, and finding the perfect playlist—lo-fi for deep focus, tempo-based for momentum. I'm driven by curiosity, strategy, and a love for building things that matter—both in code and in life.",
+  },
+  {
+    Timestamp: "2025/05/13 11:16:08 AM GMT+5:30",
+
+    Name: "Atchutha Rao T A",
+    Bio: "Im an individual pursuing B.E from the branch of AI&DS studing in the 8th semester of the course. Im passionate about AI and iv built some basic apps using python.",
+  },
+  {
+    Timestamp: "2025/05/13 11:16:40 AM GMT+5:30",
+
+    Name: "Varunkumar M S",
+    Bio: "Varunkumar is a final-year B.Tech student in Artificial Intelligence and Data Science at Siddaganga Institute of Technology, with a strong foundation in machine learning, deep learning, and data structures. He has worked on various AI-driven projects, including EEG data classification using CNN-LSTM models and fashion recommendation systems using Retrieval-Augmented Generation (RAG). Varunkumar is passionate about sequential data modeling and has hands-on experience with TensorFlow, PyTorch, and scikit-learn. With a background in both academic research and practical implementation, he aims to contribute to real-world applications of AI, particularly in natural language processing and cognitive analysis.",
+  },
+  {
+    Timestamp: "2025/05/13 11:16:40 AM GMT+5:30",
+
+    Name: "Unnati Umesh Kulkarni",
+    Bio: "A passionate Web Developer with a strong foundation in C++ programming and an avid problem-solver, always up for challenges. Specializing in designing dynamic and user-friendly web applications, I am continuously expanding my skills through platforms like Leetcode to enhance my algorithmic and coding proficiency. With a background in Computer Science Engineering (CSE), I have a deep understanding of computer systems, software development, and data structures. I'm always driven to improve my skills and solve real-world problems using the latest technologies.",
+  },
+  {
+    Timestamp: "2025/05/13 11:16:48 AM GMT+5:30",
+
+    Name: "Vaibhav G",
+    Bio: "Vaibhav was a  member of the Survey Corps. He was the main protagonist of Attack on Titan. He lived in Shiganshina District with his parents until the fall of Wall Maria, where he impotently witnessed his mother being eaten by a Titan. This event would lead to Eren's intense hatred towards the Titans as he swore to wipe all of them off the face of the Earth.  Soon afterward, his father, Grisha Yeager, found him and gave him the key to his basement, instructing Eren to find it at all costs and retake Wall Maria.He then injected Eren with a Titan serum.",
+  },
+  {
+    Timestamp: "2025/05/13 11:16:54 AM GMT+5:30",
+
+    Name: "Charanya A",
+    Bio: "I'm an aspiring full-stack developer driven by a deep curiosity for how things work behind the scenes. Currently on a journey of continuous learning, I’m building and refining my skills one project at a time. I enjoy solving real problems, experimenting with ideas, and transforming concepts into intuitive, functional digital experiences.  For me, every bug fixed and every feature built is a step forward in becoming a better developer. Beyond code, I'm passionate about creativity—whether it’s through sketching, exploring different forms of art, or simply finding inspiration in good music and meaningful conversations. I value staying curious, staying connected, and always pushing myself to grow.",
+  },
+  {
+    Timestamp: "2025/05/13 11:17:03 AM GMT+5:30",
+
+    Name: "Mohan Krishna A",
+    Bio: "I'm Mohan Krishna A, a passionate web developer with a strong foundation in full-stack development. I enjoy building clean, responsive, and functional web applications using technologies like Next.js, TypeScript, Hono, and PostgreSQL. With a solid understanding of data structures, algorithms, and backend systems, I strive to create efficient and user-friendly solutions. I'm also experienced with tools like Git, Postman, and shadcn/ui, and constantly work to improve my skills and stay up to date with the latest in web development.  Outside of coding, I enjoy exploring new tech trends, watching sci-fi movies, playing strategy games, and listening to music. These hobbies help me stay creative and balanced in my work.",
+  },
+  {
+    Timestamp: "2025/05/13 11:17:12 AM GMT+5:30",
+
+    Name: "Yogananda K S",
+    Bio: "I am Yogananda K S, a final-year Information Science & Engineering student at Siddaganga Institute of Technology, Tumakuru. With a solid foundation in programming languages like C++, Python, and SQL, I am passionate about building efficient and scalable software solutions. My academic journey is complemented by hands-on experience in projects such as potato plant disease detection using machine learning and deep learning techniques, reflecting my interest in applying technology to solve real-world problems.  In addition to my technical skills, I hold certifications in Data Structures and Algorithms (C++) and Cisco CCNA modules, enhancing my knowledge in both software development and networking fundamentals. I am an active learner, driven by curiosity and a commitment to continuous growth. Outside academics, I’ve participated in community service through Youth for Seva and represented my district in kabaddi.  Currently, I am seeking internship and entry-level opportunities where I can contribute meaningfully while continuing to learn and grow as a software engineer.",
+  },
+  {
+    Timestamp: "2025/05/13 11:17:48 AM GMT+5:30",
+
+    Name: "S TEJA",
+    Bio: "I’m S Teja, an engineering student from Tumakuru, Karnataka, currently pursuing my Bachelor of Engineering in Artificial Intelligence and Data Science at Siddaganga Institute of Technology, with a CGPA of 9.08. I have a strong academic background, having completed my pre-university education at Sri Saptagiri PU College with a 76.5% score. I'm skilled in programming languages like C/C++ and Python, and I have hands-on experience with databases such as MySQL and MongoDB. My technical foundation is built on key concepts like Object-Oriented Programming, Operating Systems, Unix, and Data Structures and Algorithms.  Over the course of my academic journey, I’ve worked on several projects that reflect my interest in practical problem-solving. These include a Telugu news classification model using NLP and transformers, a machine learning model to predict energy consumption for better grid management, a blood bank management system developed using Microsoft SQL Server, and an Airbnb market analysis using K-means clustering.  I’ve also participated in the Academy of Excellence’s Advanced Manufacturing Technology programs and have enjoyed representing my college in various cricket tournaments. I hold certifications in Internet of Things and Robotics, Python Essentials by Cisco, DSA from Geeks for Geeks, and STEP by The Hindu Group. I’m fluent in English, Kannada, and Telugu, and I’m passionate about building intelligent systems that make a meaningful impact in the real world.",
+  },
+  {
+    Timestamp: "2025/05/13 11:18:37 AM GMT+5:30",
+
+    Name: "Aditya H",
+    Bio: "I am a Computer Science student with a strong passion for web development and cloud technologies. I love building scalable, efficient, and user-friendly applications that solve real-world problems. My interests lie in full-stack development, cloud infrastructure, and machine learning.  One of my recent projects involved developing a brain tumor detection system using convolutional neural networks. I built a web-based interface with Streamlit and integrated TensorFlow Lite to enable real-time, accurate predictions from MRI images.  I’ve also worked on a Hospital Management System that uses a robust DBMS to streamline patient data handling and support better healthcare decision-making. Another project involved integrating Amazon S3 with AWS Transcribe to automate speech-to-text conversion using cloud services.  I constantly seek opportunities to grow by exploring new technologies and contributing to impactful projects. I enjoy working in collaborative environments and am eager to apply my skills to meaningful challenges in tech.",
+  },
+  {
+    Timestamp: "2025/05/13 11:18:51 AM GMT+5:30",
+
+    Name: "Divya Rani K",
+    Bio: "I am an AID student currently pursuing my degree at SIT. I'm passionate about coding and actively practice problem-solving on platforms like LeetCode and GeeksforGeeks. My technical skills include C++, DBMS, and full-stack development. Outside of coding, I enjoy reading comics—it helps me relax and sparks my creativity.",
+  },
+  {
+    Timestamp: "2025/05/13 11:18:53 AM GMT+5:30",
+
+    Name: "Akshay K",
+    Bio: "STUDENTA highly enthusiastic and hard-working individual with astrong commitment to personal and professional growth. I’mthe type of person who, if asked a question and I don’t knowthe answer, will be honest about it—what sets me apart is mydrive to find the answer. Fueled by a strong passion fortechnology, I am always ready to learn and tackle newchallenges head-on.",
+  },
+];
